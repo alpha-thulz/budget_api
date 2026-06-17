19396248e84c3dcea6b94cbbb250b773174eacdd
Alpha-Thulz <tj.tyaphile@gmail.com>
2026-06-17 23:06:26 +0200

    Added HealthController to check server status

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace budget_api.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<string> Get() => Ok("Server is up and running!");
+}
