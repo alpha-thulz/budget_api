9e77f4fc0a568056d68aa8f16af6fcb988eaf4a0
Alpha-Thulz <tj.tyaphile@gmail.com>
2026-06-17 22:17:32 +0200

    Setup enums

---
+++ Enums/CategoryGroup.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BudgetApi.Models;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum CategoryGroup
+{
+    Income = 1,
+    Housing,
+    Food,
+    Transport,
+    Healthcare,
+    Financial,
+    Other
+}
---
+++ Enums/TransactionType.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BudgetApi.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum TransactionType
+{
+    Income = 1,
+    Expense,
+    Transfer
+}
