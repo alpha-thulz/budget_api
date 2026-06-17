3703d329d6e67bc04323abb34979b74b803e5c81
Alpha-Thulz <tj.tyaphile@gmail.com>
2026-06-17 22:18:47 +0200

    Middleware setup

---
+++ Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using BudgetApi.Exceptions;
+
+namespace BudgetApi.Middleware;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (TransactionNotFoundException e)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+
+            await context.Response.WriteAsJsonAsync(new
+            {
+                error = e.Message
+            });
+        }
+        catch (Exception e)
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            await context.Response.WriteAsJsonAsync(new
+            {
+                error = "An unexpected error has occurred."
+            });
+        }
+    }
+}
